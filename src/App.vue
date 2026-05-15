6ef87d47fd5aa2bda26ff2fb71b4e57bcdceebd8
TiteTiff <simonchautemps.tiffanie@gmail.com>
2026-05-15 17:13:58 +0200

    mise en place projet Vue + Vite + TailwindCSS

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+
+<template>
+  <div class="min-h-screen bg-[#120E0B] flex items-center justify-center">
+    <h1 class="text-[#F0DFC8] text-4xl font-medium">
+      Tailwind fonctionne ! 🎉
+    </h1>
+  </div>
+</template>
